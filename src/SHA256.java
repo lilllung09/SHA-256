fbf1df079835af9b29ce2d35de5af9fde154aef9
lilllung09 <lilllung09@gmail.com>
2022-01-23 00:11:14 +0900

    Change parsing chunk size 8 -> 32 bit Add Message Expansion function (MEXP_chunk32)

--- src/SHA256.java
+++ src/SHA256.java
@@ -4,7 +4,8 @@ public class SHA256 {
     public static void main(String[] args) {
         SHA256 m = new SHA256();
 
-        m.getHashValue("abcd");
+        //m.getHashValue("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        m.getHashValue("abc");
     }
 
     public SHA256() {
@@ -15,13 +16,13 @@ public class SHA256 {
     public String getHashValue(String message) {
         String hashed_message = "";
 
-        byte[][][] message_padded = this.padding_M(message);
+        int[][] M = this.init_M(message);
 
         return hashed_message;
     }
 
-    public long multiple = 4294967296L;
-    public double[] prime_numbers = {
+    private final long multiple = 4294967296L;
+    private final double[] prime_numbers = {
         2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
         31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
         73, 79, 83, 89, 97, 101, 103, 107, 109, 113,
@@ -30,13 +31,13 @@ public class SHA256 {
         233, 239, 241, 251, 257, 263, 269, 271, 277, 281,
         283, 293, 307, 311
     };
-    public long[] H = new long[8];
-    public long[] K = new long[64];
+    private final long[] H = new long[8];
+    private final long[] K = new long[64];
 
     //For SHA-256 initial hash value
     private void init_H() {
         for (int i = 0; i < 8; i++)
-            H[i] = (long)((Math.sqrt(prime_numbers[i]) % 1) * multiple);
+            H[i] = (long) ((Math.sqrt(prime_numbers[i]) % 1) * multiple);
     }
     //SHA-256 Constants
     private void init_K() {
@@ -44,22 +45,30 @@ public class SHA256 {
             K[i] = (long)((Math.cbrt(prime_numbers[i]) % 1) * multiple);
     }
 
-    public byte[][][] padding_M(String message) {
-        byte[] mb = message.getBytes(StandardCharsets.UTF_8);
+    private int[][] init_M(String message) {
+        final byte[] mb = message.getBytes(StandardCharsets.UTF_8);
 
         // message의 bit 수
         long bit_size = mb.length * 8L;
 
-        // 1. message의 길이를 512bit의 배수로 만들 공간
-        byte[][][] message_P = new byte[mb.length / 64 + 1 + (bit_size % 512 + 1 > 448 ? 1 : 0)][16][4];
+        // 생성될 512bit 블럭 갯수
+        final int length_block_512 = mb.length / 64 + (bit_size % 512 > 448 - 1 ? 1 : 0) + 1;
 
-        // 2. message를 message_P에 넣기 (앞 부분 부터)
-        for (int i = 0, pos = 0; i < message_P.length && pos < mb.length; i++) {
-            for (int j = 0; j < message_P[0].length && pos < mb.length; j++)
-                for (int k = 0; k < message_P[0][0].length && pos < mb.length; k++, pos++)
-                    message_P[i][j][k] = mb[pos];
+        ////////////////////////////////////////////////////////////////
+        // 1. Message Expansion Function을 거쳐 반환할 message_M
+        ////////////////////////////////////////////////////////////////
+        int[][] message_M = new int[length_block_512][64];
+
+
+        ////////////////////////////////////////////////////////////////
+        // 2. message를 message_M으로 옮기기 (chunk 변경, byte -> int)
+        ////////////////////////////////////////////////////////////////
+        for (int i = 0, pos = 0; i < length_block_512 && pos < mb.length; i++) {
+            for (int j = 0; j < 16 && pos < mb.length; j++)
+                for (int k = 3; k > -1 && pos < mb.length; k--, pos++)
+                    message_M[i][j] |= mb[pos] << (8 * k);
         }
-        // 확인용: message를 message_P에 넣기
+        // 확인용: message를 message_M으로 옮기기
         {
             System.out.println("-------------------------origin message input-------------------------");
             for (byte b : mb)
@@ -70,59 +79,59 @@ public class SHA256 {
                         + " "
                 );
             System.out.println();
-            for (int i = 0, pos = 0; i < message_P.length && pos < mb.length; i++) {
-                for (int j = 0; j < message_P[0].length && pos < mb.length; j++)
-                    for (int k = 0; k < message_P[0][0].length && pos < mb.length; k++, pos++)
-                        System.out.println(
-                                String.format("%32s", Integer.toBinaryString(message_P[i][j][k]))
-                                        .substring(24)
-                                        .replace(' ', '0')
-                                + ", message_P[" + i + "][" + j + "][" + k + "]"
-                        );
-            }
-        }
-
-        // 3. 추가된 zero-bit와 message를 구분하기 위해 '0b1000_0000' 넣기
-        message_P[message_P.length - 1][(int)bit_size % 512 / 32][(int)bit_size % 512 % 32 / 8] = (byte)0b1000_0000;
-
-        // 4. message의 사이즈를 message_P의 끝 64bit에 넣기 (뒷 부분 부터)
-        for (int j = message_P[0].length - 1; j > 13; j--) {
-            for (int k = message_P[0][0].length - 1; k > -1; k--) {
-                message_P[message_P.length - 1][j][k] = (byte) bit_size;
-                bit_size = bit_size >> 8;
-            }
-        }
-        // 확인용: message의 사이즈를 message_P의 끝 64bit에 넣기
-        {
-            System.out.println("-------------------------origin message size-------------------------");
-            System.out.println(String.format("%64s", Long.toBinaryString(mb.length * 8L)).replace(' ', '0'));
-            for (int i = 14; i < message_P[0].length; i++) {
-                for (int j = 0; j < message_P[0][0].length; j++)
+            for (int i = 0, pos = 0; i < length_block_512 && pos < mb.length; i++) {
+                for (int j = 0; j < 16 && pos < mb.length; j++, pos += 4)
                     System.out.println(
-                            String.format("%32s", Integer.toBinaryString(message_P[message_P.length - 1][i][j]))
-                                    .substring(24)
+                            String.format("%32s", Integer.toBinaryString(message_M[i][j]))
                                     .replace(' ', '0')
-                            + ", message_P[" + (message_P.length - 1) + "][" + i + "][" + j + "]"
+                            + ", message_P[" + i + "][" + j + "]"
                     );
             }
+        }
+
+
+        ////////////////////////////////////////////////////////////////
+        // 3. 추가된 zero-bit와 message를 구분하기 위해 '1' 넣기
+        ////////////////////////////////////////////////////////////////
+        message_M[(int)bit_size / 512][(int)bit_size % 512 / 32] |= 0b1000_0000_0000_0000_0000_0000_0000_0000 >>> mb.length % 4 * 8;
+
+
+        ////////////////////////////////////////////////////////////////
+        // 4. message의 사이즈를 message_M의 끝 64bit에 넣기 (뒷 부분 부터)
+        ////////////////////////////////////////////////////////////////
+        message_M[length_block_512 - 1][15] = (int)bit_size;
+        message_M[length_block_512 - 1][14] = (int)(bit_size >>> 32);
+
+
+        ////////////////////////////////////////////////////////////////
+        // 5. Message Expansion Function 적용
+        ////////////////////////////////////////////////////////////////
+        for (int[] block_512 : message_M) {
+            for (int j = 16; j < 64; j++)
+                block_512[j] = MEXP_chunk32(block_512[j-2], block_512[j-7], block_512[j-15], block_512[j-16]);
         }
 
 
         // 결과
         {
             System.out.println("-------------------------padding message output-------------------------");
-            for (byte[][] block_512 : message_P) {
-                for (byte[] block_32 : block_512)
-                    for (byte block_8 : block_32)
-                        System.out.print(
-                                String.format("%32s", Integer.toBinaryString(block_8))
-                                        .substring(24)
-                                        .replace(' ', '0')
-                                + " "
-                        );
+            for (int[] block_512 : message_M) {
+                for (int j = 0; j < 16; j++)
+                    System.out.print(
+                            String.format("%32s", Integer.toBinaryString(block_512[j]))
+                                    .replace(' ', '0')
+                            + " "
+                    );
+                System.out.println();
             }
         }
+        return message_M;
+    }
 
-        return message_P;
+    private int MEXP_chunk32(int block_2, int block_7, int block_15, int block_16) {
+        return (block_2 >>> 7 ^ block_2 >>> 18 ^ block_2 >> 3)  // σ1
+                + block_7
+                + (block_15 >>> 7 ^ block_15 >>> 18 ^ block_15 >> 3) // σ0
+                + block_16;
     }
 }
