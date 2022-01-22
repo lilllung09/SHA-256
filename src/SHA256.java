27ad902856112ed5d683828e20c8e560e0a4a092
lilllung09 <lilllung09@gmail.com>
2022-01-23 03:39:52 +0900

    Works with "abc"! (The End? No!)

--- src/SHA256.java
+++ src/SHA256.java
@@ -4,8 +4,7 @@ public class SHA256 {
     public static void main(String[] args) {
         SHA256 m = new SHA256();
 
-        //m.getHashValue("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-        m.getHashValue("abc");
+        System.out.println(m.getHashString("abc"));
     }
 
     public SHA256() {
@@ -13,12 +12,25 @@ public class SHA256 {
         this.init_K();
     }
 
-    public String getHashValue(String message) {
-        String hashed_message = "";
+    public String getHashString(String message) {
+        StringBuilder hashed_sb = new StringBuilder();
 
+        // Padding and Expansion the Message
         int[][] M = this.init_M(message);
 
-        return hashed_message;
+        // Round Function
+        int[] H = H0.clone();
+        for (int[] block_512 : M) {
+            for (int i = 0; i < 64; i++)
+                H = this.Round_Function(this.K[i], H, block_512[i]);
+        }
+
+
+        for (int i = 0; i < 8; i++) {
+            hashed_sb.append(String.format("%X", H[i] + H0[i]));
+        }
+
+        return hashed_sb.toString().replace(' ', '0');
     }
 
     private final long multiple = 4294967296L;
@@ -31,20 +43,21 @@ public class SHA256 {
         233, 239, 241, 251, 257, 263, 269, 271, 277, 281,
         283, 293, 307, 311
     };
-    private final long[] H = new long[8];
-    private final long[] K = new long[64];
+    private final int[] H0 = new int[8];
+    private final int[] K = new int[64];
 
-    //For SHA-256 initial hash value
+    // SHA-256 initial hash value
     private void init_H() {
         for (int i = 0; i < 8; i++)
-            H[i] = (long) ((Math.sqrt(prime_numbers[i]) % 1) * multiple);
+            H0[i] = (int)(long) ((Math.sqrt(prime_numbers[i]) % 1) * multiple);
     }
-    //SHA-256 Constants
+    // SHA-256 Constants
     private void init_K() {
         for (int i = 0; i < 64; i++)
-            K[i] = (long)((Math.cbrt(prime_numbers[i]) % 1) * multiple);
+            K[i] = (int)(long) ((Math.cbrt(prime_numbers[i]) % 1) * multiple);
     }
 
+    // Padding the Message
     private int[][] init_M(String message) {
         final byte[] mb = message.getBytes(StandardCharsets.UTF_8);
 
@@ -69,7 +82,7 @@ public class SHA256 {
                     message_M[i][j] |= mb[pos] << (8 * k);
         }
         // 확인용: message를 message_M으로 옮기기
-        {
+        /*{
             System.out.println("-------------------------origin message input-------------------------");
             for (byte b : mb)
                 System.out.print(
@@ -87,7 +100,7 @@ public class SHA256 {
                             + ", message_P[" + i + "][" + j + "]"
                     );
             }
-        }
+        }*/
 
 
         ////////////////////////////////////////////////////////////////
@@ -108,12 +121,11 @@ public class SHA256 {
         ////////////////////////////////////////////////////////////////
         for (int[] block_512 : message_M) {
             for (int j = 16; j < 64; j++)
-                block_512[j] = MEXP_chunk32(block_512[j-2], block_512[j-7], block_512[j-15], block_512[j-16]);
+                block_512[j] = MEXP(block_512[j - 2], block_512[j - 7], block_512[j - 15], block_512[j - 16], j);
         }
 
-
         // 결과
-        {
+        /*{
             System.out.println("-------------------------padding message output-------------------------");
             for (int[] block_512 : message_M) {
                 for (int j = 0; j < 16; j++)
@@ -124,14 +136,37 @@ public class SHA256 {
                     );
                 System.out.println();
             }
-        }
+        }*/
         return message_M;
     }
 
-    private int MEXP_chunk32(int block_2, int block_7, int block_15, int block_16) {
-        return (block_2 >>> 7 ^ block_2 >>> 18 ^ block_2 >> 3)  // σ1
+    // Message Expansion Function
+    private int MEXP(int block_2, int block_7, int block_15, int block_16, int j) {
+        return ((Integer.rotateRight(block_2, 17)) ^ (Integer.rotateRight(block_2, 19)) ^ (block_2 >>> 10))  // σ1
                 + block_7
-                + (block_15 >>> 7 ^ block_15 >>> 18 ^ block_15 >> 3) // σ0
+                + ((Integer.rotateRight(block_15, 7)) ^ (Integer.rotateRight(block_15, 18)) ^ (block_15 >>> 3)) // σ0
                 + block_16;
+    }
+
+    // Round Function
+    private int[] Round_Function(int k, int[] H, int w) {
+        int upper_sigma_1 = (Integer.rotateRight(H[4], 6)) ^ (Integer.rotateRight(H[4], 11)) ^ (Integer.rotateRight(H[4], 25)); // Σ1
+        int choose = (H[4] & H[5]) ^ (~H[4] & H[6]);
+
+        return new int[] {
+                ((Integer.rotateRight(H[0], 2)) ^ (Integer.rotateRight(H[0], 13)) ^ (Integer.rotateRight(H[0], 22))) // Σ0
+                + ((H[0] & H[1]) ^ (H[0] & H[2]) ^ (H[1] & H[2])) // Maj(a1, b1, c1)
+                + (k + upper_sigma_1 + choose + H[7] + w),
+
+                H[0], // a1 -> b2
+                H[1], // b1 -> c2
+                H[2], // c1 -> d2
+
+                H[3] + (k + upper_sigma_1 + choose + H[7] + w),
+
+                H[4], // e1 -> f2
+                H[5], // f1 -> g2
+                H[6], // g1 -> h2
+        };
     }
 }
