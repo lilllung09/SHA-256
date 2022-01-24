1886c1e6f6f9541b64ceb742643b5ece44484261
lilllung09 <lilllung09@gmail.com>
2022-01-24 18:26:43 +0900

    Add result of test

--- src/SHA256_Sample.java
+++ src/SHA256_Sample.java
@@ -7,8 +7,10 @@ public class SHA256_Sample {
 
         // Test with string
         System.out.println(m.getHashFromString(new String("Hello World!")));
+        // 7F83B1657FF1FC53B92DC18148A1D65DFC2D4B1FA3D677284ADDD200126D9069
 
         // Test with File
-        System.out.println(m.getHashFromFile("sample.pdf"));
+        System.out.println(m.getHashFromFile("NIST.FIPS.180-4.pdf"));
+        // 0455B406D89648D20CBDE375561E19C245B9815E894164C2670772E3D54DEB82
     }
 }
